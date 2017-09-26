dce8445d8b3136dc9a6db8b579fe053b2e753be8
gitpash <arklogin@gmail.com>
2017-09-26 11:42:35 +0300

    add lazy fetching

--- schema.js
+++ schema.js
@@ -16,11 +16,11 @@ const BookType = new GraphQLObjectType({
   fields: () => ({
     title: {
       type: GraphQLString,
-      resolve: xml => xml.title[0]
+      resolve: xml => xml.GoodreadsResponse.book[0].title[0]
     },
     isbn: {
       type: GraphQLString,
-      resolve: xml => xml.isbn[0]
+      resolve: xml => xml.GoodreadsResponse.book[0].isbn[0]
     }
   })
 });
@@ -36,8 +36,14 @@ const AuthorType = new GraphQLObjectType({
     },
     books: {
       type: new GraphQLList(BookType),
-      resolve: xml => 
-        xml.GoodreadsResponse.author[0].books[0].book
+      resolve: xml => { 
+        const ids = xml.GoodreadsResponse.author[0].books[0].book.map(elem => elem.id[0]._) //._ cause xml2js syntax
+        return Promise.all(ids.map(id =>
+          fetch(`https://www.goodreads.com/book/show/${id}.xml?key=ljFm6TbUYCNt3I6eRdXrPQ`)
+            .then(response => response.text())
+            .then(parseXML)
+        ))
+      }
     }
   })
 });
